a39ceec055bc5222f53c25a29e654bebfdad5c23
falme <falme_streamless@hotmail.com>
2015-07-17 17:55:47 -0300

    Iniciado

---
+++ script.js
@@ -0,0 +1,48 @@
+var ativo =false;
+
+document.getElementById('TabPT').onclick = function(){
+	document.getElementById('TabPT').className="Tab selected";
+	document.getElementById('TabPX').className="Tab";
+	document.getElementById('pttopx').style.display="block";
+	document.getElementById('pxtopt').style.display="none";
+	document.getElementById('Titulo').innerHTML="Points to Pixels";
+	document.title = "Pt->Px"
+}
+
+document.getElementById('TabPX').onclick = function(){
+	document.getElementById('TabPT').className="Tab";
+	document.getElementById('TabPX').className="Tab selected";
+	document.getElementById('pttopx').style.display="none";
+	document.getElementById('pxtopt').style.display="block";
+	document.getElementById('Titulo').innerHTML="Pixels to Points";
+	document.title = "Px->Pt"
+}
+
+document.getElementById('FormulaActive').onclick = function(){
+
+	if(ativo)
+	{
+		document.getElementById('FormulaContent').style.height="0px";
+		document.getElementById('FormulaContent').style.padding="0px";
+		document.getElementById('FormulaContent').style.borderWidth="0px";
+		ativo = false;
+	} else {
+		document.getElementById('FormulaContent').style.height="100px";
+		document.getElementById('FormulaContent').style.padding="20px";
+		document.getElementById('FormulaContent').style.borderWidth="1px";
+		ativo = true;
+	}
+}
+
+function PtPx()
+{
+	var resultado = document.getElementById('Pt2Px_Pt').value * 96 / 72;
+	document.getElementById('ResultadoPx').innerHTML = resultado+" Pixels";
+}
+
+function PxPt()
+{
+	var resultado = document.getElementById('Px2Pt_Px').value * 72 / 96;
+	document.getElementById('ResultadoPt').innerHTML = resultado+" Points";
+}
+
